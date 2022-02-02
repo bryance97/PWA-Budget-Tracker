83855aa14eb7b045a68c176ec574519c3a9ba4ed
bryance97 <bryancifuentesescobar@gmail.com>
2022-02-02 06:53:56 -0500

    fetch saved data when connection exists

--- public/js/idb.js
+++ public/js/idb.js
@@ -25,3 +25,34 @@ function saveRecord(record) {
 
   store.add(record);
 }
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {        
+          return response.json();
+        })
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+      }
+    };
+  }
+  
+  // listen for app coming back online
+  window.addEventListener("online", checkDatabase);
